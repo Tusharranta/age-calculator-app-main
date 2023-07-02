b8f25f07efdfe8c01231d4b7210f19338222fde4
Tusharranta <rantatushar45@gmail.com>
2023-07-03 00:00:21 +0530

    age-calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+const dayInp = document.getElementById("day");
+const monthInp = document.getElementById("month");
+const yearInp = document.getElementById("year");
+
+const dayOtp = document.getElementById("DD");
+const monthOtp = document.getElementById("MM");
+const yearOtp = document.getElementById("YY");
+
+const form = document.querySelector("form");
+
+const date = new Date();
+let day = date.getDate();
+let month = 1 + date.getMonth();
+let year = date.getFullYear();
+
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
